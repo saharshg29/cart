09121378a51c336bc43e2de6c67ffff3ca8dcf36
saharshg29 <saharshg29@gmail.com>
2022-02-19 17:59:05 +0530

    setting up redux

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import "./App.css";
 import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./Components/Nav";
+import { Provider } from "react-redux";
+import Store from "./Store";
+
+import { BrowserRouter as Browser, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Home></Home>
+      <Browser>
+        <Provider store={Store}>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            {/* <Route path="/cart" element={<Home />}></Route> */}
+          </Routes>
+        </Provider>
+      </Browser>
     </div>
   );
 }
---
+++ src/reducer/Reducer.jsx
@@ -0,0 +1,49 @@
+import {
+    initialState,
+    CREATE_TASK,
+    READ_TASK,
+    UPDATE_TASK,
+    DELETE_TASK,
+  } from "../constants";
+  
+  export function Reducer(state = initialState, action) {
+    switch (action.type) {
+      case CREATE_TASK: {
+        return { ...state, todo: [action.payload, ...state.todo] };
+      }
+  
+      case READ_TASK:
+        let arr = state.todo.filter((task) => task.id == action.payload);
+        arr = arr.values();
+        for (let val of arr) {
+          arr = val;
+        }
+        return {
+          ...state,
+          task: arr,
+        };
+  
+      case UPDATE_TASK: {
+        console.log('edit reducer',action.payload)
+        return {
+          ...state,
+          todo: state.todo.map((task) =>
+            task.id == action.payload.id ? action.payload : task
+          ),
+        };
+      }
+  
+      case DELETE_TASK :
+        console.log("Delete is getting called", action);
+        return {
+          ...state,
+          todo: state.todo.filter(
+            (task) => task.id != action.payload
+          )
+        }
+  
+  
+      default:
+        return state;
+    }
+  }
