d9c4a7bc9198a7215978975c478f656900cd0b8c
saharshg29 <saharshg29@gmail.com>
2022-02-20 01:33:03 +0530

    Bootstrap to nav applied

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Nav() {
   const navigate = useNavigate();
-  const selected = useSelector(store => store.todo.selected)
-  console.log(selected)
+  const selected = useSelector((store) => store.todo.selected);
+  console.log(selected);
   return (
     <>
-      <nav className="nav mt-3">
-        <span className="nav-link active">Active</span>
-        <span className="nav-link">Link</span>
-        <span className="nav-link">Link</span>
-        <span className="nav-link disabled">Disabled</span>
-        <button
-          type="button"
-          onClick={() => navigate("/cart")}
-          className="btn btn-primary position-relative d-flex"
-        >
-          Cart
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {selected.length}
-            <span className="visually-hidden">unread messages</span>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span onClick={() => navigate("/")} className="navbar-brand">
+            Home
           </span>
-        </button>
+
+          <button
+            type="button"
+            onClick={() => navigate("/cart")}
+            className="btn btn-primary position-relative right-20 "
+          >
+            Cart
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {selected.length}
+              <span className="visually-hidden">unread messages</span>
+            </span>
+          </button>
+        </div>
       </nav>
     </>
   );
