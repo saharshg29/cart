643aafd5a0193c2713f46aec7ccf9c899ea05f98
saharshg29 <saharshg29@gmail.com>
2022-02-20 00:37:29 +0530

    Change in read reducer

--- src/reducer/Reducer.jsx
+++ src/reducer/Reducer.jsx
@@ -1,49 +1,42 @@
 import {
-    initialState,
-    CREATE_TASK,
-    READ_TASK,
-    UPDATE_TASK,
-    DELETE_TASK,
-  } from "../constants";
-  
-  export function Reducer(state = initialState, action) {
-    switch (action.type) {
-      case CREATE_TASK: {
-        return { ...state, selected: [action.payload, ...state.selected] };
-      }
-  
-      case READ_TASK:
-        let arr = state.todo.filter((task) => task.id == action.payload);
-        arr = arr.values();
-        for (let val of arr) {
-          arr = val;
-        }
-        return {
-          ...state,
-          task: arr,
-        };
-  
-      case UPDATE_TASK: {
-        console.log('edit reducer',action.payload)
-        return {
-          ...state,
-          todo: state.todo.map((task) =>
-            task.id == action.payload.id ? action.payload : task
-          ),
-        };
-      }
-  
-      case DELETE_TASK :
-        console.log("Delete is getting called", action);
-        return {
-          ...state,
-          todo: state.todo.filter(
-            (task) => task.id != action.payload
-          )
-        }
-  
-  
-      default:
-        return state;
+  initialState,
+  CREATE_TASK,
+  READ_TASK,
+  UPDATE_TASK,
+  DELETE_TASK,
+} from "../constants";
+
+export function Reducer(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_TASK: {
+      return { ...state, selected: [action.payload, ...state.selected] };
     }
+
+    case READ_TASK:
+      let arr = state.selected
+      return {
+        ...state,
+        selected: arr,
+      };
+
+    case UPDATE_TASK: {
+      console.log("edit reducer", action.payload);
+      return {
+        ...state,
+        todo: state.todo.map((task) =>
+          task.id == action.payload.id ? action.payload : task
+        ),
+      };
+    }
+
+    case DELETE_TASK:
+      console.log("Delete is getting called", action);
+      return {
+        ...state,
+        todo: state.todo.filter((task) => task.id != action.payload),
+      };
+
+    default:
+      return state;
   }
+}
