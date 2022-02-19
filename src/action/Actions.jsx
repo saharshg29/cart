9574531e08a6e9927f6f00717740c26872d23abb
saharshg29 <saharshg29@gmail.com>
2022-02-19 18:22:20 +0530

    Redux setup complete

---
+++ src/action/Actions.jsx
@@ -0,0 +1,21 @@
+import { CREATE_TASK, READ_TASK, UPDATE_TASK, DELETE_TASK } from "../constants";
+
+export const Add = (Task) => ({
+  type: CREATE_TASK,
+  payload: Task,
+});
+
+export const Get = (id) => ({
+  type: READ_TASK,
+  payload: id,
+});
+
+export const EditTask = (Task) => ({
+  type: UPDATE_TASK,
+  payload: Task,
+});
+
+export const DeleteTask = (id) => ({
+  type: DELETE_TASK,
+  payload: id,
+});
